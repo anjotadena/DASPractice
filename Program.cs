84400ddf10157c793fc90cf42ba2a48a538e4ede
Anjo Tadena <tadena.anjo@gmail.com>
2024-12-13 07:34:23 +0800

    Retry until user press esc to exit the application

--- Program.cs
+++ Program.cs
@@ -1,9 +1,4 @@
-﻿using DSAPractice.ArrayAndHashing;
-using DSAPractice.BitManipulation;
-using DSAPractice.LinkedList;
-using DSAPractice.Recursion;
-using DSAPractice.Stack;
-using System.Reflection;
+﻿using System.Reflection;
 
 namespace DSAPractice;
 
@@ -11,30 +6,57 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Data structure and Algorithms | Practice");
+        Console.WriteLine("Welcome to my Data structure and Algorithms Collections");
+
+        var exit = false;
 
         // get all solutions
+        do
+        {
+            var solutionTypes = GetAllProblems();
+
+            Console.Write("Select a solution to execute (enter the number): ");
+
+            if (!int.TryParse(Console.ReadLine(), out int choice) || choice < 0 || choice >= solutionTypes.Count)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Invalid selection! Please select [1-{solutionTypes.Count}]");
+                Console.ResetColor();
+                continue;
+            }
+            ISolution selectedSolution = (ISolution)Activator.CreateInstance(solutionTypes[choice - 1])!;
+
+            Console.WriteLine("-------------------ANSWER------------------");
+            Console.WriteLine(selectedSolution.Answer());
+
+            Console.Write("\nDo you want to try another problem again? [Any key to continue or Esc to exit] ");
+
+            ConsoleKey key = Console.ReadKey(true).Key;
+
+            if (key != ConsoleKey.Enter)
+            {
+                Console.WriteLine("\nExiting application. Goodbye!");
+                exit = true;
+            } else
+            {
+                Console.Clear();
+            }
+        } while (!exit);
+    }
+
+    static private IList<Type> GetAllProblems()
+    {
         var solutionsTypes = Assembly.GetExecutingAssembly()
             .GetTypes()
             .Where(t => typeof(ISolution).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract)
             .ToList();
 
-        for (int i = 0; i< solutionsTypes.Count; i++)
+        for (int i = 0; i < solutionsTypes.Count; i++)
         {
             Console.WriteLine($"{i + 1}. {solutionsTypes[i].Name}");
         }
 
-        Console.WriteLine("Select a solution to execute (enter the number): ");
-
-        if (int.TryParse(Console.ReadLine(), out int choice) && choice > 0 && choice <= solutionsTypes.Count)
-        {
-            ISolution selectedSolution = (ISolution)Activator.CreateInstance(solutionsTypes[choice - 1])!;
-
-            Console.WriteLine(selectedSolution.Answer());
-        }
-        else
-        {
-            Console.WriteLine("Invalid selection!");
-        }
+        return solutionsTypes;
     }
 }
